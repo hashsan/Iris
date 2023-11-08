226c12f54c4beb5da16f9d3f05e649e433ce7a7a
はっしゅさん <149641384+hashsan@users.noreply.github.com>
2023-11-08 20:32:29 +0900

    Update Iris.js

--- Iris.js
+++ Iris.js
@@ -4,114 +4,69 @@ with @import url(https://hashsan.github.io/fujiyama/fujiyama.css?v=3);
   
 */
 
-
-
-function make(ask,ary,ans){  
-  const render = (ary) =>{
-    return ary.map((d,i)=>{
-      if(!ans){
-        return `<p><input placeholder="${d}"></p>`
-      }
-      return `<p><input value="${ans[i]}" placeholder="${d}"></p>`
-    }).join('\n')    
-  }
-
-
-  var temp=`
-<form id="Iris" class="pbox"
-      style="position:fixed;right:2rem;top:2rem;">
-  <div class="wrap">
-    <p class="ask" style="padding-bottom:0.5rem">${ask}</p>
-    ${render(ary)}
-    <p class="c"><button>submit</button></p>    
-  </div>
-</form>`
-
-
-  var el = document.createElement('div')
-  el.innerHTML=temp  
-  document.body.append(el.children[0])
-  el = void 0;
-  return document.getElementById('Iris')
-}
-
-function isString(str){
-  return typeof str === "string";
-}
-
 class Iris{
-  //el;
-  //ask;
-  //ary;
-  //key;
-  //submit
-  ///
-  constructor(){
-    
-  }
-  ///////
-  talk=(ask,ary,key)=>{return new Promise((sol)=>{
-    ary = ary||[]
-    this.ask = ask
-    this.ary = isString(ary)?[ary]:ary
-    this.key = key
-    
-    this.el=this.make()
-    //console.log('el?',this.el)    
-    this.submit = document.querySelector('#Iris button')
-    this.submit.onclick =(e)=>{
-      e.preventDefault()
-      this.ans = this.inputs().map(d=>d.value)
-      //console.log(this.key,this.ans)
-      this.setCache(this.key,this.ans)
-      this.remove()
-      sol(this.ans)
-    }
-  })}
-  getCache=(key)=>{
-    if(!key){
-      return
-    }
-    var json = localStorage.getItem(key)
-    //console.log(json)
-    if(!json){
-      return
-    }
-    return JSON.parse(json)
-  }
+  ans;  
+  talk=(ask,sample,key)=>{return new Promise(sol=>{
+    const fn={}
+    fn.q=d=>document.querySelector(d)
+    const $=this
 
-  
-  inputs(){
-    var els = document.querySelectorAll('#Iris input')
-    return Array.from(els)
-  }
-  setCache(key,ary){
+    $.query = '#Iris'
+    $.q_ask = $.query+' .ask'
+    $.q_ans = $.query+' .ans'
+    $.q_ok  = $.query+' .ok'
+    $.el = $.make(ask,sample,key)
+    //console.log($.background)
+
+    fn.q($.q_ok).onclick = (e) =>{
+      e.preventDefault();//form submit block
+      $.ans = fn.q($.q_ans).value
+      $.remove()
+      $.set(key,$.ans)
+      sol($.ans)
+    }
+
+  })}
+
+  get=(key)=>{
     if(!key){
       return
     }
-    var json = JSON.stringify(ary)
-    localStorage.setItem(key,json)
+    return localStorage.getItem(key)
   }
-  make(){
-    var {ask,ary,key}=this
-    return make(ask,ary,this.getCache(key))
+  //////////////
+  set(key,ans){
+    if(!key){
+      return
+    }
+    localStorage.setItem(key,ans)
+  }
+  make(ask,sample,key){
+    const disa=()=>{
+      if(sample){
+        return
+      }
+      return 'disabled'
+    }
+    var temp=`
+<form id="Iris" class="pbox">
+  <p class="ask">${ask}</p>
+  <p><input class="ans" type="text"
+  placeholder="${sample||''}"
+  value="${this.get(key)||''}"
+  ${disa()}
+  ></p>
+  <p class="c"><button class="ok">submit</button></p>
+</form>
+    `.trim()
+    var el = document.createElement('div')
+    el.innerHTML=temp
+    el = el.children[0];
+    document.body.append(el)
+    return el;
   }
   remove(){
     this.el.remove()
   }
-
 }
 
-window.Iris = Iris
-
-/*usage
-
-var iris =new Iris()
-
-iris.talk('tokenとジットハブのレポ',['ghp_token','repo_url'],'xyz')
- .then(d=>{
-  
-  console.log('ans',d)
-  iris.talk(`ghp -> ${d.at(0)}`)
-})
-*/
